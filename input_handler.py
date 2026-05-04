4f8285fca9f1fe023152929b96330f44c36c2b71
emmanpestan <emmanuel.empestan@gmail.com>
2026-05-05 02:15:16 +0800

    Added try again input option

--- input_handler.py
+++ input_handler.py
@@ -10,14 +10,18 @@ class InputHandler:
         choice = input("Choose an operation: ")
 
         if choice not in ["1","2","3","4","5"]:
-            raise ValueError("Select only from the the operation choice")
+            raise ValueError("Select only from the operation choices")
     
         return choice
     
-    def get_numbers(self, message):
+    def get_number(self, message):
         while True:
             try:
                 num = float(input(message))
                 return num
             except ValueError:
                 print("Invalid input. Please enter a valid number.")
+
+    def try_again(self):
+        choice = input("Do you want to perform another calculation? (y/n): ")
+        return choice.lower() == 'y'
