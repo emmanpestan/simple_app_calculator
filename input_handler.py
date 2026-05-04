689ade0c1b001ac58bcb85013f9866bbf3ed5bae
emmanpestan <emmanuel.empestan@gmail.com>
2026-05-05 01:56:43 +0800

    Added input handler class with operation options

---
+++ input_handler.py
@@ -0,0 +1,16 @@
+class InputHandler:
+    def get_operation(self):
+        print("\n Calculator via python")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Exponent")
+
+        choice = input("Choose an operation: ")
+
+        if choice not in ["1","2","3","4","5"]
+            raise ValueError("Select only from the the operation choice")
+    
+        return choice
+    
